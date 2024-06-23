f02733611afaaac453691dfc28b1ed0755a9146f
TitikshaBansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-06-23 10:30:23 +0530

    Update SumOfsquareNumbers.cpp

--- Medium/SumOfsquareNumbers.cpp
+++ Medium/SumOfsquareNumbers.cpp
@@ -1,1 +1,20 @@
-
+#include<cmath>
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long a=0, b= sqrt(c);
+        while(a<=b){
+            long long sum=(a*a)+(b*b);
+            if(sum == c){
+                return true;
+            }
+            else if(sum<c){
+                a++;
+            }
+            else{
+                b--;
+            }
+        }
+        return false;
+    }
+};
