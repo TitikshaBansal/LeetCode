39e8845d2fea59aaa4644e105a3670ca0ab9320d
TitikshaBansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-06-27 17:15:32 +0530

    Create FindTheDivisibilityArrayOfAString.cpp

---
+++ Medium/FindTheDivisibilityArrayOfAString.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> divisibilityArray(string word, int m) {
+        vector<int> ans;
+        long long num=0;
+        for(int i=0; i< word.length(); i++){
+            num= (num*10+ (word[i]-'0'))% m  ;
+            if(num==0) ans.push_back(1);
+            else ans.push_back(0);
+        }
+        return ans;
+    }
+};
