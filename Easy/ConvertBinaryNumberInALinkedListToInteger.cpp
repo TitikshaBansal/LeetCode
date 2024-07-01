9058748ee0605ed19763ce13db2489c3fb62ec9a
TitikshaBansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-07-01 17:02:54 +0530

    Update ConvertBinaryNumberInALinkedListToInteger.cpp

--- Easy/ConvertBinaryNumberInALinkedListToInteger.cpp
+++ Easy/ConvertBinaryNumberInALinkedListToInteger.cpp
@@ -10,3 +10,34 @@ public:
         return ans;
     }
 };
+
+************************************************************************************************************************************************************************************************************************
+second Approach
+class Solution {
+
+private:
+    void reverse(ListNode* &head){
+        ListNode* pre= nullptr;
+        ListNode* curr= head;
+        ListNode* next= nullptr;
+        while(curr!=nullptr){
+            next= curr->next;
+            curr->next= pre;
+            pre= curr;
+            curr= next;
+        }
+        head= pre;
+    }
+public:
+    int getDecimalValue(ListNode* head) {
+        reverse(head);
+        int ans=0;
+        int mul=1;
+        while(head!=nullptr){
+            ans+= head->val*mul;
+            mul*=2;
+            head= head->next;
+        }
+        return ans;
+    }
+};
