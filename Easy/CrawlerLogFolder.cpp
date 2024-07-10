8359816a0c4a247693ce98f5b8824bf05e6dce79
TitikshaBansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-07-10 11:31:32 +0530

    Create CrawlerLogFolder.cpp

---
+++ Easy/CrawlerLogFolder.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+        for (const auto& log : logs) {
+            if (log == "./") continue;
+            else if (log == "../") {
+                if (depth > 0) depth--;  
+            } 
+            else depth++;
+        }
+        return depth;
+    }
+};
