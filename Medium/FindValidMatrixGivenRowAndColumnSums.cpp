f75cb4752deccaaf4cbd3cd4b0daa371098ad2e0
TitikshaBansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-07-20 11:28:24 +0530

    Create FindValidMatrixGivenRowAndColumnSums.cpp

---
+++ Medium/FindValidMatrixGivenRowAndColumnSums.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        vector<vector<int>> ans(rowSum.size(), vector<int>(colSum.size(), 0));
+        int i = 0, j = 0;
+        while (i < rowSum.size() && j < colSum.size()) {
+            int val = min(rowSum[i], colSum[j]);
+            ans[i][j] = val;
+            rowSum[i] -= val;
+            colSum[j] -= val;
+
+            if (rowSum[i] == 0) {
+                i++;
+            }
+            if (colSum[j] == 0) {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
