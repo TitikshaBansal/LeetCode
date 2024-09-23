ca0afb3877cd9ab437057d50d74584bbe5e5be9f
TitikshaBansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-09-24 01:15:46 +0530

    Create fibonacciNumber.cpp

---
+++ Easy/fibonacciNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n < 2) return n;
+        int ans;
+        int a = 0, b = 1;
+        for(int i = 2 ; i <= n ; ++i){
+            ans = a + b ;
+            a = b ;
+            b = ans ;
+        }
+        return ans;
+    }
+};
+
+// https://leetcode.com/problems/fibonacci-number/
