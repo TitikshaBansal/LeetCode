52b84ee7129f68462fa6e5b980e690a4ad157770
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-09-24 20:35:11 +0530

    added optimized solution FindDuplicateNumber.cpp

--- Medium/FindDuplicateNumber.cpp
+++ Medium/FindDuplicateNumber.cpp
@@ -11,3 +11,25 @@ public:
         return -1;
     }
 };
+
+
+// optimized solution
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        fast = nums[0];
+        while(slow != fast){
+            slow= nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
+// https://leetcode.com/problems/find-the-duplicate-number/
