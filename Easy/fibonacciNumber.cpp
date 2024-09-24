d75d3dad625509bc75462795bebb4a912d8e8e5a
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-09-25 00:58:41 +0530

    added new approach fibonacciNumber.cpp

--- Easy/fibonacciNumber.cpp
+++ Easy/fibonacciNumber.cpp
@@ -13,4 +13,15 @@ public:
     }
 };
 
+
+// recursive approach
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        if(n<3) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+};
+
 // https://leetcode.com/problems/fibonacci-number/
