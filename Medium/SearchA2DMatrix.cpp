ca2abb92bac5385f33fd69ce14106ccc682478fe
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-09-25 16:32:45 +0530

    Update SearchA2DMatrix.cpp

--- Medium/SearchA2DMatrix.cpp
+++ Medium/SearchA2DMatrix.cpp
@@ -12,4 +12,23 @@ public:
     }
 };
 
+// one more approach
+// but earlier is better imo
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int s = 0;
+        int e = matrix.size()*matrix[0].size()-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            int row = mid/matrix[0].size();
+            int col = mid%matrix[0].size();
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) e = mid-1;
+            else s= mid+1;
+        }
+        return false;
+    }
+};
+
 // https://leetcode.com/problems/search-a-2d-matrix/
