f6af8eb328f307a5fb0b408c624427f318f3b019
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-09-25 17:05:28 +0530

    Update MajorityElement.cpp

--- Easy/MajorityElement.cpp
+++ Easy/MajorityElement.cpp
@@ -6,3 +6,20 @@ public:
         return nums[mid];
     }
 };
+
+// better approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count =0;
+        int ele;
+        for(int num : nums){
+            if(count == 0)  ele = num;
+            if(ele == num) count++;  
+            else count--;
+        }
+        return ele;
+    }
+};
+
+// https://leetcode.com/problems/majority-element/
