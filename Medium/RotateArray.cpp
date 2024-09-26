b69ee033efd7a4d52851328305fa0290625aae2d
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-09-27 00:40:18 +0530

    Update RotateArray.cpp

--- Medium/RotateArray.cpp
+++ Medium/RotateArray.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        while(k>nums.size()) k-= nums.size();
+        k%=nums.size();
+        if(k>nums.size()) return;
+        //while(k>nums.size()) k-= nums.size();
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin()+k);
         reverse(nums.begin()+k, nums.end());
