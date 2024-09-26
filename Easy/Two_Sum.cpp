72a58cbb0ed814ad1f237c4abfe078a4aa21577b
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2024-09-26 19:59:59 +0530

    Update Two_Sum.cpp

--- Easy/Two_Sum.cpp
+++ Easy/Two_Sum.cpp
@@ -7,5 +7,22 @@ vector<int> twoSum(vector<int>& nums, int target) {
             }
         }
         return {};
-        
     }
+
+// better approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map; 
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end()) {
+                return {map[complement], i}; 
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+// https://leetcode.com/problems/two-sum/
