c8441e2cb5cec4f23e7f15bbf3188be40d8e19c4
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-01-10 11:31:16 +0530

    Create wordSubsets.cpp

---
+++ Medium/wordSubsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        int freq[26] = {};
+        for (string word : words2){
+            int count[26] = {};
+            for (char c : word) count[c - 'a']++;
+            for (int i = 0; i < 26; i++) freq[i] = max(freq[i], count[i]);
+        }
+        vector<string> res;
+        for (auto& word : words1){
+            int count[26] = {}, i;
+            for (char c : word) count[c - 'a']++;
+            for (i = 0; i < 26; i++)
+                if (count[i] < freq[i])
+                    break;
+            if (i == 26) res.push_back(word);
+        }
+        return res;
+    }
+};
+
+// https://leetcode.com/problems/word-subsets/?envType=daily-question&envId=2025-01-10
