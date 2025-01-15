4e7d022f99a750dd4d715eb1117abff2a78864e9
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-01-15 19:37:43 +0530

    Create findSubsequenceOfLengthKWithLargestSum.cpp

---
+++ Easy/findSubsequenceOfLengthKWithLargestSum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto i : nums){
+            pq.push(i);
+            if(pq.size() > k ) pq.pop();
+        }   
+        unordered_map<int, int> mp;
+        while( !pq.empty() ) {
+            mp[pq.top()]++;
+            pq.pop();
+        }
+        vector<int> v;
+        for(auto i : nums) {
+            if( mp[i] > 0 ){
+                v.push_back(i);
+                mp[i]--;
+            }
+        }
+        return v;
+    }
+};
+
+// https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
