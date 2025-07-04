ff61114e58bf2ffe74dde354435f63b1c30f6113
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-07-04 16:57:52 +0530

    Create findKthCharacterInStringGameII.cpp

---
+++ Hard/findKthCharacterInStringGameII.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        int ans = 0;
+        int t;
+        while (k != 1) {
+            t = __lg(k);
+            if (((long long)1 << t) == k) {
+                t--;
+            }
+            k = k - ((long long)1 << t);
+            if (operations[t]) {
+                ans++;
+            }
+        }
+        return 'a' + (ans % 26);
+    }
+};
+
+// https://leetcode.com/problems/find-the-k-th-character-in-string-game-ii/?envType=daily-question&envId=2025-07-04
